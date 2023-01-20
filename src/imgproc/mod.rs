2234d674ba196bbcb128d62125939746fca16c30
reifujimura <fujimura@chordizm.com>
2023-01-20 09:55:06 +0900

    Impl imgproc filter2D

--- src/imgproc/mod.rs
+++ src/imgproc/mod.rs
@@ -1,11 +1,13 @@
 mod bilateral;
 mod consts;
 mod cvt_color;
+mod filter;
 mod find_contours;
 mod threshold;
 
 pub use bilateral::*;
 pub use consts::*;
 pub use cvt_color::*;
+pub use filter::*;
 pub use find_contours::*;
 pub use threshold::*;
